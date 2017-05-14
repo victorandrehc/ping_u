fa6b4e8913ae1d07770d76030f6119b1ac9d9791
Victor Costa <victorandrehc@gmail.com>
2017-05-14 10:25:46 +0200

    assign2_2

--- assignment2/assignment2_2/assignment2_2.ino
+++ assignment2/assignment2_2/assignment2_2.ino
@@ -7,7 +7,8 @@ const unsigned int neben_amp_gel=30;
 const unsigned int neben_amp_grun=32;
 const unsigned int bewegung_sensor=40;
 
-unsigned int8_t state=1;
+unsigned int state=1;
+unsigned int bewegung=0;
 unsigned long t=0;
 
 void grun_h();
@@ -19,6 +20,9 @@ void grun_n();
 void gelb_n();
 void rot_n();
 void rot_gelb_n();
+
+unsigned long deltat();
+void update_bewegung();
 
 
 void setup() {
@@ -37,17 +41,92 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- switch (state){
-     case 1:
-       
-       break;
+  Serial.print("STATE: ");
+  Serial.println(state);
+  Serial.print("BEWEGUNG?: ");
+  Serial.println(bewegung);
+  switch (state){
+    case 1:
+      grun_h();
+      rot_n();
+      if (bewegung==1 || digitalRead(bewegung_sensor)==HIGH){
+        state=2;
+        bewegung=0;
+        t=millis();
+      }else{
+        state=1;
+      }
+      break;
 
-     default:
-       state=1;
-       Serial.println("RESETING FSM");
- }
+    case 2:
+      gelb_h();
+      if (deltat()>=1000){
+        state=3;
+      }else{
+        state=2;
+      }
+      break;
+
+    case 3:
+      rot_h();
+      if(deltat()>=2000){
+        state=4;
+      }else{
+        state=3;
+      }
+      break;
+
+    case 4:
+      rot_gelb_n();
+      if(deltat()>=3000){
+        state=5;
+      }
+      break;
+
+    case 5:
+      grun_n();
+      if(deltat()>=8000){
+        state=6;
+      }
+      break;
+    case 6:
+      gelb_n();
+      if(deltat()>=9000){
+        state=7;
+      }
+      break;
+
+    case 7:
+      rot_n();
+      update_bewegung();
+      if (deltat()>=10000){
+        state=8;
+      }
+      break;
+    case 8:
+      rot_gelb_h();
+      update_bewegung();
+      if(deltat()>=11000){
+        t=millis();
+        state=9;
+      }
+      break;
+    case 9:
+      grun_h();
+      update_bewegung();
+      if(deltat()>=20000){
+        state=1;
+      }
+      break;
+    default:
+      state=1;
+      Serial.println("RESETING FSM");
+  }
 }
 
+unsigned long deltat(){
+  return millis()-t;
+}
 void grun_h(){
   digitalWrite(haupt_amp_rot, LOW);
   digitalWrite(haupt_amp_gel, LOW);
@@ -89,6 +168,26 @@ void rot_gelb_n(){
   digitalWrite(neben_amp_gel, HIGH);
   digitalWrite(neben_amp_grun, LOW);
 }
+
+void update_bewegung(){
+  if(bewegung!=1){
+    bewegung=digitalRead(bewegung_sensor);
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
