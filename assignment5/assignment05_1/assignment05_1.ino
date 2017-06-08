0de29233c615bb501701ea134652e31d56ae8b08
Victor Costa <victorandrehc@gmail.com>
2017-06-08 15:46:20 +0200

    inserting pins

--- assignment5/assignment05_1/assignment05_1.ino
+++ assignment5/assignment05_1/assignment05_1.ino
@@ -1,10 +1,18 @@
 const int SIZE=6;
+const unsigned long t_debounce=50;
+const int button_left=23;
+const int button_right=25;
+
+const int LED[6]={7,6,5,4,3,2};
+
+unsigned long t=0;
 
 int input_1[SIZE],input_2[SIZE],output[SIZE];
 
 
 int add(int* output,int* input_1,int* input_2,int size);
 void print_array(int* arr,int size);
+unsigned long deltat();
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,21 +20,21 @@ void setup() {
     input_1[i]=0;
     input_2[i]=0;
     output[i]=0;
+    pinMode(LED[i], OUTPUT);
+    digitalWrite(LED[i], HIGH);
   }
-  input_1[3]=1;
-  input_1[4]=1;
-  input_1[5]=1;
-
-  input_2[4]=1;
-  input_2[5]=1;
+  pinMode(button_left,INPUT);
+  pinMode(button_right,INPUT);
   Serial.begin(9600);
+  t=millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  add(output,input_1,input_2,SIZE);
+  /*add(output,input_1,input_2,SIZE);
   print_array(output,SIZE);
-  delay(500);
+  delay(500);*/
+  Serial.println(digitalRead(button_right));
 }
 
 int add(int* output,int* input_1,int* input_2,int size){
@@ -45,6 +53,15 @@ void print_array(int* arr,int size){
   }
   Serial.println("");
 }
+
+unsigned long deltat(){
+  return millis()-t;
+}
+
+
+
+
+
 
 
 
