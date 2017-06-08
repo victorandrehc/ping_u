c4ef103983d24da3a8fec6abd6ce61119046b1c1
Victor Costa <victorandrehc@gmail.com>
2017-06-08 14:29:35 +0200

    complete somatorin series to solve the problem of binary sum

---
+++ assignment5/sum_bin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#SUM A+B
+
+
+def sum_bin(a,b):
+	N=len(a)
+	result=[0 for i in range(N)]
+	i=N-1
+	aux=0
+	while i>=0:
+		result[i]=a[i]^b[i]^aux
+		aux= (a[i] and b[i]) or (aux and (a[i]^b[i]))
+		i-=1
+	return (result,aux)
+
+a=[0,0,0,1,1,1]
+b=[0,0,0,0,1,1]
+
+print sum_bin(a,b)
